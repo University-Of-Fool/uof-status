cc69af4c8376ae5e165d34cc88a7b10bcf06e482
Tienyu Yang <53754724+lingrottin@users.noreply.github.com>
2023-02-06 14:28:10 +0800

    build.sh hotfix

--- build.sh
+++ build.sh
@@ -4,5 +4,3 @@ cd $(dirname $0)
 npm install
 npx tsc
 cp -r ./package.json ./layouts ./prisma ./config.toml ./README.md ./LICENSE ./APIREADME.md build/
-# without using -i option for Darwin and *BSD
-sed -e's|"node build/app"|"node app"|' build/package.json > build/package.json
